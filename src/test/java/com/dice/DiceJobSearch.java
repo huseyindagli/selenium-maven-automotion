03faadfe0b128cf5f7ed65db99003141ca3f8582
hd <dglhuseyin@gmail.com>
2018-06-13 17:36:21 -0500

    print test

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -55,6 +56,7 @@ public class DiceJobSearch {
 		}
 		
 		driver.close();
+		System.out.println("TEST COMPLETED - "+LocalDateTime.now());
 	}
 
 }
